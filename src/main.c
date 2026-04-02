48fb89e2f63eddebd6332d93dce5095df4b56246
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2026-04-02 20:29:27 +0200

    feat: write object to database

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "repository.h"
+#include "object.h"
+#include <stdlib.h>
 
 int main(int ac, char **av)
 {
-    create_repository();
+    bob_object_t *obj = object_new("blob", "Hello, this is another file!!", 29);
+    char *digest = object_write(obj);
+    free(digest);
+    object_free(obj);
     return 0;
 }
