44d3251394ed6fe188f5087d5014ea0f39cdd5ff
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2026-04-04 20:18:22 +0200

    mod: create config file when initializing repo

--- src/repository.c
+++ src/repository.c
@@ -28,19 +28,35 @@ static int create_dirs(int mode)
     return 0;
 }
 
+static int create_head_file(void)
+{
+    FILE *fp = fopen(".bob/HEAD", "w");
+    if (fp == NULL) {
+        fprintf(stderr, "Couldn't create bob HEAD: %s\n", strerror(errno));
+        return -1;
+    }
+    fprintf(fp, "ref: refs/heads/main\n");
+    fclose(fp);
+}
+
+static int create_config_file(void)
+{
+    FILE *fp = fopen(".bob/config", "w");
+    if (fp == NULL) {
+        fprintf(stderr, "Couldn't create bob config file: %s\n", strerror(errno));
+        return -1;
+    }
+    fclose(fp);
+}
+
 int create_repository(void)
 {
     if (create_dirs(0755) == -1) {
         return -1;
     }
-
-    FILE *fp = fopen(".bob/HEAD", "w");
-    if (fp == NULL) {
-        perror("create_repository");
+    if (create_head_file() == -1 || create_config_file() == -1) {
         return -1;
     }
-    fprintf(fp, "ref: refs/heads/main\n");
-    fclose(fp);
     char abspath[4096] = {0};
     if (realpath(".bob", abspath) == NULL) {
         return -1;
