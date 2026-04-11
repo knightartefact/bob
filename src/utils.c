a6ba5df6f9dcab444b2e1e6dd3e2b9e82ce33608
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2026-04-11 19:15:13 +0200

    feat: add support for multiple parents in a commit

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,16 @@
 #include "utils.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void sha2hex(const unsigned char *digest, char *out)
 {
     for (int i = 0; i < 20; i++)
         sprintf(out + i * 2, "%02x", digest[i]);
     out[40] = '\0';
+}
+
+int sha_equal(const unsigned char *a, const unsigned char *b)
+{
+    return memcmp(a, b, 20) == 0;
 }
