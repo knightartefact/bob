fd40a8f537cbb628fa6714c7be12c49209a49a49
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2026-04-14 20:37:55 +0200

    feat: implement cat_tree function to display tree objects

--- src/commands.c
+++ src/commands.c
@@ -51,6 +51,49 @@ int cmd_hash_object(const char *filepath)
     return 0;
 }
 
+static void cat_blob(const bob_object_t *blob)
+{
+    fwrite(blob->data, sizeof(char), blob->size, stdout);
+}
+
+static void cat_tree(const bob_object_t *tree)
+{
+    size_t i = 0;
+
+    while (i < tree->size) {
+        // Parse mode (up to space)
+        const char *mode = tree->data + i;
+        const char *space = memchr(mode, ' ', tree->size - i);
+        if (space == NULL)
+            break;
+        size_t mode_len = space - mode;
+
+        // Parse name (up to null byte)
+        const char *name = space + 1;
+        const char *nul = memchr(name, '\0', tree->size - (name - tree->data));
+        if (nul == NULL)
+            break;
+        size_t name_len = nul - name;
+
+        // 20-byte binary SHA1 follows the null byte
+        const unsigned char *sha = (const unsigned char *)(nul + 1);
+        if ((sha + 20) - (const unsigned char *)tree->data > (long)tree->size)
+            break;
+
+        char hex[41] = {0};
+        sha2hex(sha, hex);
+
+        // Determine type from mode: "40000" is tree, otherwise blob
+        const char *type = (mode_len == 5 && memcmp(mode, "40000", 5) == 0)
+            ? "tree" : "blob";
+
+        printf("%.*s %s %s\t%.*s\n",
+            (int)mode_len, mode, type, hex, (int)name_len, name);
+
+        i = (sha + 20) - (const unsigned char *)tree->data;
+    }
+}
+
 int cmd_cat_file(const char *hex)
 {
     bob_object_t *obj = object_read(hex);
@@ -59,8 +102,10 @@ int cmd_cat_file(const char *hex)
         return -1;
     }
     printf("type: %s\n", obj->type);
-    fwrite(obj->data, sizeof(char), obj->size, stdout);
-    printf("\n");
+    if (strcmp(obj->type, "tree") == 0)
+        cat_tree(obj);
+    else
+        cat_blob(obj);
 
     object_free(obj);
     return 0;
